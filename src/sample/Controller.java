b726df6c54ed21a4b8d4e6997813e45647afe260
nihsa2907 <Hansa29062537@hotmail.com>
2018-02-23 11:59:49 +0700

    login

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,100 @@
 package sample;
 
-public class Controller {
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    public loginModel loginmodel = new loginModel();
+
+    public Controller() {
+    }
+    @FXML
+    private JFXTextField username;
+
+    @FXML
+    private JFXTextField password;
+
+    @FXML
+    private JFXButton btnLogin;
+
+    @FXML
+    private Label dbStatus;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        if (this.loginmodel.isDatabaseConnection()){
+            this.dbStatus.setText("Connection to DB.");
+        }else {
+            this.dbStatus.setText("Not Connect to DB.");
+        }
+
+    }//initialize
+    @FXML
+    public void Login (ActionEvent event){
+        try {
+            if
+                    (this.loginModel.isLogin(this.username.getText(),this.password.getText(),
+                    this.combobox.getValue().toString())){
+                Stage stage = (Stage) this.btnLogin.getScene().getWindow();
+                stage.close();
+                switch (this.combobox.getValue().toString()){
+                    case "Admin": adminLogin(); break;
+                    case "Student": studentLogin(); break;
+                }
+            }else{
+                this.loginStatus.setText("Wrong Credential");
+            }
+        } catch (Exception localException) {
+            localException.printStackTrace();
+        }
+    }
+    public void studentLogin(){
+        try {
+            Stage userStage = new Stage();
+            FXMLLoader loader = new FXMLLoader();
+            Pane root = (Pane)
+                    loader.load(getClass().getResource("/Students/StudentFXML.fxml").openStream
+                            ());
+            StudentsController studentsController = (StudentsController)
+                    loader.getController();
+            Scene scene = new Scene(root);
+            userStage.setScene(scene);
+            userStage.setTitle("Student Dashboard");
+            userStage.setResizable(false);
+            userStage.show();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    public void adminLogin(){
+        try {
+            Stage adminStage = new Stage();
+            FXMLLoader adminLoader = new FXMLLoader();
+            Pane adminroot = (Pane)
+                    adminLoader.load(getClass().getResource("/Admin/Admin.fxml").openStream());
+            AdminController adminController = (AdminController)
+                    adminLoader.getController();
+            Scene scene = new Scene(adminroot);
+            adminStage.setScene(scene);
+            adminStage.setTitle("Admin Dashboard");
+            adminStage.setResizable(false);
+            adminStage.show();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
+
+}//class
---
+++ src/sample/loginModel.java
@@ -0,0 +1,43 @@
+package sample;
+
+import dbUtil.dbConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class loginModel {
+    Connection connection;
+
+    public loginModel() {
+        try {
+            this.connection = dbConnection.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        if (this.connection == null){
+            System.exit(1);
+        }
+    }//loginModel
+    public boolean isDatabaseConnection(){
+        return this.connection != null;
+    }//isDatabaseConnection
+    public boolean isLogin(String user,String pass){
+        PreparedStatement pr = null;
+        ResultSet rs = null;
+    }
+    String sql = "SELECT * FROM login where username = ? and password = ? and division = ?";
+    try {
+        try {
+            pr = this.connection.prepareStatement(sql);
+            pr.setString(1,user);
+            pr.setString(2,pass);
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }//class
