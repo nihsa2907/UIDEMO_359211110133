2a0630aec4065290b6a0e3abba5534f03b7f2ecd
nihsa2907 <Hansa29062537@gmail.com>
2018-02-25 17:35:29 +0700

    Load Student Data from SQLite

--- src/sample/loginModel.java
+++ src/sample/loginModel.java
@@ -8,6 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class loginModel {
+
     Connection connection;
 
     public loginModel() {
@@ -16,28 +17,38 @@ public class loginModel {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        if (this.connection == null){
+
+        if (this.connection == null) {
             System.exit(1);
         }
     }//loginModel
     public boolean isDatabaseConnection(){
         return this.connection != null;
     }//isDatabaseConnection
-    public boolean isLogin(String user,String pass){
+
+    public boolean isLogin(String user, String pass) throws SQLException {
         PreparedStatement pr = null;
         ResultSet rs = null;
-    }
-    String sql = "SELECT * FROM login where username = ? and password = ? and division = ?";
-    try {
+
+        String sql = "select * from user where username = ? and password = ?";
         try {
             pr = this.connection.prepareStatement(sql);
             pr.setString(1,user);
-            pr.setString(2,pass);
+            pr.setString(2, pass);
+            rs = pr.executeQuery();
 
-
+            if (rs.next()) {
+                return true;
+            }
+            return false;
         } catch (SQLException e) {
             e.printStackTrace();
+            return false;
+        }finally {
+            pr.close();
+            rs.close();
         }
 
 
-    }//class
+    }//isLogin
+}//class
